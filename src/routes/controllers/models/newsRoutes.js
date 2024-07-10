5eea43fd5ef0ce306336f6529409ff1891ebdec1
ABHISHEK ANAND <145061316+wutchermax@users.noreply.github.com>
2024-07-10 15:59:30 +0530

    Delete src directory

--- src/routes/controllers/models/newsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const newsController = require('../controllers/newsController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.get('/', authMiddleware, newsController.getNews);
-
-module.exports = router;
