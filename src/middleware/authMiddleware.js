5a1240ce41a7b26fe95d280b51bc2ed0c24386b3
ABHISHEK ANAND <145061316+wutchermax@users.noreply.github.com>
2024-07-10 16:02:10 +0530

    Create authMiddleware.js

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+module.exports = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
+  try {
+    const decoded = jwt.verify(token, config.jwtSecret);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Token is not valid' });
+  }
+};
